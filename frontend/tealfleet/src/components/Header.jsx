224f4784c4dc322cb1c838bf10ab4ec9fd6bbf57
Jakob Jozelj <jakobjozelj@protonmail.com>
2023-08-15 13:04:50 +0200

    Navigation bar changes

--- frontend/tealfleet/src/components/Header.jsx
+++ frontend/tealfleet/src/components/Header.jsx
@@ -29,7 +29,6 @@ function Header() {
         gridTemplateColumns= 'repeat(12, 1fr)'
         gridTemplateRows='repeat(2, 1fr)'>  
 
-            
             <GridItem
             colSpan={{base: 12, sm: 12, md: 12}}
             colStart={{base: 1, sm: 1, md: 1}}
@@ -60,8 +59,6 @@ function Header() {
                     </Hide>
                     <Hide breakpoint='(max-width: 800px)'>
                     <Spacer/>
-
-                    
                         <Box margin={{base: '0.5em', sm: '0.5em', md: '0.5em'}}>
                             <HeaderMainNav/>
                         </Box>
---
+++ frontend/tealfleet/src/components/HeaderMobileMenu.jsx
@@ -0,0 +1,20 @@
+// React components
+import * as React from 'react';
+
+
+// Chakra-UI components 
+import {
+    Text,
+    Flex
+  } from '@chakra-ui/react';
+  
+  function HeaderMobileMenu() {
+
+    return (
+        <Flex>
+          <Text>Sample Text</Text>
+        </Flex>
+    )
+  }
+
+  export default HeaderMobileMenu;
