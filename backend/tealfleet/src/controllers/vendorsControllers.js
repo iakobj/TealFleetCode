137fa75549f909f409c247ff5ca1c2fb4cc1363c
Jakob Jozelj <me@jakobjozelj.com>
2024-01-14 14:31:37 +0100

    vendor uses identity check

--- backend/tealfleet/src/controllers/vendorsControllers.js
+++ backend/tealfleet/src/controllers/vendorsControllers.js
@@ -4,49 +4,46 @@ const {
   vendorsGetByName,
 } = require("../services/vendorsServices");
 
-// Get all vendors
 module.exports.cVendorsGetAll = async (req, res) => {
   try {
-    const result = await vendorsGetAll();
-    res.status(200).send({"data": result});
-  } catch (err) {
-    console.log(err);
-    res.status(404).send("No vendors found");
-  }
-};
-
-// Get vendor by id
-module.exports.cVendorsGetById = async (req, res) => {
-  const id = req.params.id;
-  try {
-    const result = await vendorsGetById(id);
-    res.status(200).send({"data": result});
-  } catch (err) {
-    console.log(err);
-    res
-      .status(404)
-      .send(
-        `The vendor was not found, invalid input syntax for type uuid ${id}`
-      );
-  }
-};
-
-// Get vendor by name
-module.exports.cVendorsGetByName = async (req, res) => {
-  const name = req.params.name;
-  try {
-    const result = await vendorsGetByName(name);
-    if (result.length === 0) {
-      res
-        .status(404)
-        .send(
-          `The vendor was not found, invalid input syntax for type name ${name}`
-        );
-    } else {
+    const result = await vendorsGetAll(req);
+    if(result[0] && result[0].error) {
+      res.status(401).send({"data": result});
+    }else {
       res.status(200).send({"data": result});
     }
   } catch (err) {
     console.log(err);
-    res.status(500).send("500 Internal Server Error");
+    res.status(404).send("Vendors not found");
+  }
+};
+
+module.exports.cVendorsGetById = async (req, res) => {
+  const vendor_id = req.params.id;
+  try {
+    const result = await vendorsGetById(req, vendor_id);
+    if(result[0] && result[0].error) {
+      res.status(401).send({"data": result});
+    }else {
+      res.status(200).send({"data": result});
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(404).send("Vendor not found");
+  }
+};
+
+module.exports.cVendorsGetByName = async (req, res) => {
+  const vendor_name = req.params.name;
+  try {
+    const result = await vendorsGetByName(req, vendor_name);
+    if(result[0] && result[0].error) {
+      res.status(401).send({"data": result});
+    }else {
+      res.status(200).send({"data": result});
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(404).send("Vendor not found");
   }
 };
--- backend/tealfleet/src/services/vendorsServices.js
+++ backend/tealfleet/src/services/vendorsServices.js
@@ -1,16 +1,53 @@
 const { query } = require("../services/db/index");
+const { checkIdentity } = require("../middlewares/identity");
 
-module.exports.vendorsGetAll = async () => {
-  const result = await query("SELECT * FROM vendors");
-  return result.rows;
+module.exports.vendorsGetAll = async (req) => {
+  try {
+    const identityCheck = await checkIdentity(req);
+    const { tenant_id, tenant_root } = await identityCheck.data;
+
+    if (tenant_root == true) {
+      const result = await query("SELECT * FROM vendors");
+      return result.rows;
+    } else {
+      const result = await query("SELECT * FROM vendors");
+      return result.rows;
+    }
+  } catch (error) {
+    return [{ error: `${error}` }];
+  }
 };
 
-module.exports.vendorsGetById = async (id) => {
-  const result = await query("SELECT * FROM vendors WHERE vendor_id = $1", [id]);
-  return result.rows;
+module.exports.vendorsGetById = async (req, vendor_id) => {
+  try {
+    const identityCheck = await checkIdentity(req);
+    const { tenant_id, tenant_root } = await identityCheck.data;
+
+    if (tenant_root == true) {
+      const result = await query("SELECT * FROM vendors WHERE vendor_id = $1", [vendor_id]);
+      return result.rows;
+    } else {
+      const result = await query("SELECT * FROM vendors WHERE vendor_id = $1", [vendor_id]);
+      return result.rows;
+    }
+  } catch (error) {
+    return [{ error: `${error}` }];
+  }
 };
 
-module.exports.vendorsGetByName = async (name) => {
-  const result = await query("SELECT * FROM vendors WHERE vendor_name = $1", [name]);
-  return result.rows;
+module.exports.vendorsGetByName = async (req, vendor_name) => {
+  try {
+    const identityCheck = await checkIdentity(req);
+    const { tenant_id, tenant_root } = await identityCheck.data;
+
+    if (tenant_root == true) {
+      const result = await query("SELECT * FROM vendors WHERE vendor_name = $1", [vendor_name]);
+      return result.rows;
+    } else {
+      const result = await query("SELECT * FROM vendors WHERE vendor_name = $1", [vendor_name]);
+      return result.rows;
+    }
+  } catch (error) {
+    return [{ error: `${error}` }];
+  }
 };
