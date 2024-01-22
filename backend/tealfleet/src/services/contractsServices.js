f6b38d50ef833625ae9b7ba781068644064933a0
Jakob Jozelj <me@jakobjozelj.com>
2024-01-22 21:35:37 +0100

    moved the identity from services to controller

--- backend/tealfleet/src/services/contractsServices.js
+++ backend/tealfleet/src/services/contractsServices.js
@@ -13,12 +13,14 @@ module.exports.contractsGetAll = async (identity) => {
       `);
       return result.rows;
     } else {
-      const result = await query(`
+      const result = await query(
+        `
         SELECT * 
         FROM contracts
         JOIN tenants ON contracts.tenant_id = tenants.tenant_id
         JOIN contract_types ON contracts.contract_type_id = contract_types.contract_type_id
-        WHERE contracts.tenant_id = $1`, [tenant_id]
+        WHERE contracts.tenant_id = $1`,
+        [tenant_id]
       );
       return result.rows;
     }
@@ -32,21 +34,25 @@ module.exports.contractsGetByTenant = async (identity, ten_id) => {
     const { tenant_id, tenant_root } = await identity.data;
 
     if (tenant_root == true) {
-      const result = await query(`
+      const result = await query(
+        `
         SELECT * 
         FROM contracts
         JOIN tenants ON contracts.tenant_id = tenants.tenant_id
         JOIN contract_types ON contracts.contract_type_id = contract_types.contract_type_id
-        WHERE contracts.tenant_id = $1`, [ten_id]
+        WHERE contracts.tenant_id = $1`,
+        [ten_id]
       );
       return result.rows;
     } else {
-      const result = await query(`
+      const result = await query(
+        `
         SELECT * 
         FROM contracts
         JOIN tenants ON contracts.tenant_id = tenants.tenant_id
         JOIN contract_types ON contracts.contract_type_id = contract_types.contract_type_id
-        WHERE contracts.tenant_id = $1`, [tenant_id]
+        WHERE contracts.tenant_id = $1`,
+        [tenant_id]
       );
       return result.rows;
     }
@@ -68,12 +74,14 @@ module.exports.hwContractsGetAll = async (identity) => {
     `);
       return result.rows;
     } else {
-      const result = await query(`
+      const result = await query(
+        `
       SELECT *
       FROM hw_asset_contracts
       JOIN contracts ON hw_asset_contracts.contract_id = contracts.contract_id
       JOIN hardware_assets ON hw_asset_contracts.hardware_asset_id = hardware_assets.hardware_asset_id;
-      WHERE contracts.tenant_id = $1`, [tenant_id]
+      WHERE contracts.tenant_id = $1`,
+        [tenant_id]
       );
       return result.rows;
     }
@@ -95,12 +103,14 @@ module.exports.swContractsGetAll = async (identity) => {
       `);
       return result.rows;
     } else {
-      const result = await query(`
+      const result = await query(
+        `
         SELECT *
         FROM sw_asset_contracts
         JOIN contracts ON sw_asset_contracts.contract_id = contracts.contract_id
         JOIN software_assets ON sw_asset_contracts.software_asset_id = software_assets.software_asset_id;
-        WHERE contracts.tenant_id = $1`, [tenant_id]
+        WHERE contracts.tenant_id = $1`,
+        [tenant_id]
       );
       return result.rows;
     }
@@ -114,21 +124,25 @@ module.exports.swContractsGetByContractNo = async (identity, contract_no) => {
     const { tenant_id, tenant_root } = await identity.data;
 
     if (tenant_root == true) {
-      const result = await query(`
+      const result = await query(
+        `
       SELECT *
       FROM sw_asset_contracts
       JOIN contracts ON sw_asset_contracts.contract_id = contracts.contract_id
       JOIN software_assets ON sw_asset_contracts.software_asset_id = software_assets.software_asset_id
-      WHERE contracts.contract_no = $1; `, [contract_no]
+      WHERE contracts.contract_no = $1; `,
+        [contract_no]
       );
       return result.rows;
     } else {
-      const result = await query(`
+      const result = await query(
+        `
       SELECT *
       FROM sw_asset_contracts
       JOIN contracts ON sw_asset_contracts.contract_id = contracts.contract_id
       JOIN software_assets ON sw_asset_contracts.software_asset_id = software_assets.software_asset_id
-      WHERE contracts.contract_no = $1 AND contracts.tenant_id = $2`, [contract_no, tenant_id]
+      WHERE contracts.contract_no = $1 AND contracts.tenant_id = $2`,
+        [contract_no, tenant_id]
       );
       return result.rows;
     }
@@ -138,16 +152,35 @@ module.exports.swContractsGetByContractNo = async (identity, contract_no) => {
 };
 
 module.exports.hwContractsGetByContractNo = async (identity, contract_no) => {
-  const result = await query(
-    `
-  SELECT *
-  FROM hw_asset_contracts
-  JOIN contracts ON hw_asset_contracts.contract_id = contracts.contract_id
-  JOIN hardware_assets ON hw_asset_contracts.hardware_asset_id = hardware_assets.hardware_asset_id
-  WHERE contracts.contract_no = $1;`,
-    [contract_no]
-  );
-  return result.rows;
+  try {
+    const { tenant_id, tenant_root } = await identity.data;
+
+    if (tenant_root == true) {
+      const result = await query(
+        `
+        SELECT *
+        FROM hw_asset_contracts
+        JOIN contracts ON hw_asset_contracts.contract_id = contracts.contract_id
+        JOIN hardware_assets ON hw_asset_contracts.hardware_asset_id = hardware_assets.hardware_asset_id
+        WHERE contracts.contract_no = $1;`,
+        [contract_no]
+      );
+      return result.rows;
+    } else {
+      const result = await query(
+        `
+        SELECT *
+        FROM hw_asset_contracts
+        JOIN contracts ON hw_asset_contracts.contract_id = contracts.contract_id
+        JOIN hardware_assets ON hw_asset_contracts.hardware_asset_id = hardware_assets.hardware_asset_id
+        WHERE contracts.contract_no = $1 AND contracts.tenant_id = $2`,
+        [contract_no, tenant_id]
+      );
+      return result.rows;
+    }
+  } catch (error) {
+    return [{ error: error }];
+  }
 };
 
 // TODO sw 1
