d93b2a035df30901aea0b3960dfc400ca68a65f0
Jakob Jozelj <me@jakobjozelj.com>
2024-09-16 17:44:52 +0200

    apiEndpoint for local and remote dev enviroment

--- frontend/tealfleet/src/constants/apiEndpoint.js
+++ frontend/tealfleet/src/constants/apiEndpoint.js
@@ -1,1 +1,6 @@
-export const API_ENDPOINT = "https://dev.tealfleet.com/api";
+
+// Use this only for local dev enviroment
+export const API_ENDPOINT = "https://localhost/api";
+
+// DEV & PROD version use HTTPS
+//export const API_ENDPOINT = "https://dev.tealfleet.com/api";
