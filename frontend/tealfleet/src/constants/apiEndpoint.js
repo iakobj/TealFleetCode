a643765d80807f7c2d27eb30fd23e48c51e0e37a
Jakob Jozelj <me@jakobjozelj.com>
2024-10-19 21:43:58 +0200

    deployment fixes

--- frontend/tealfleet/src/constants/apiEndpoint.js
+++ frontend/tealfleet/src/constants/apiEndpoint.js
@@ -1,6 +1,6 @@
 
 // Use this only for local dev enviroment
-//export const API_ENDPOINT = "https://localhost/api";
+export const API_ENDPOINT = "https://localhost/api";
 
 // DEV & PROD version use HTTPS
-export const API_ENDPOINT = "https://dev.tealfleet.com/api";
+//export const API_ENDPOINT = "https://dev.tealfleet.com/api";
